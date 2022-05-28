36988bbc13d113e48dfcb2bf5b2d38a67a8dd532
Daniel Smitarello <dsmitarello@gmail.com>
2022-05-28 00:19:22 -0300

    1er commit

--- Familia/views.py
+++ Familia/views.py
@@ -4,17 +4,9 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def fecha_actual(request):
-    fecha = datetime.now().date()
-    mensaje = f"Hoy es {fecha} !!"
-    return HttpResponse(mensaje)
 
-
-def saludo(request):
-    return HttpResponse("Buenas... es la página de mi familia.")
-
-def probando_templates(request):
+def index(request):
     contex = {
         "date": datetime.now(),
     }
-    return render(request, "template1.html", context = contex)
+    return render(request, "index.html", context = contex)
